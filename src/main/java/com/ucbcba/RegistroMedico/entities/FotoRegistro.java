728a5b1a24a447d655a6a2071acda87d065021b4
Julia1297 <valita129@gmail.com>
2018-05-05 08:46:37 -0400

    Entidades commit

--- src/main/java/com/ucbcba/RegistroMedico/entities/FotoRegistro.java
+++ src/main/java/com/ucbcba/RegistroMedico/entities/FotoRegistro.java
@@ -1,4 +1,21 @@
 package com.ucbcba.RegistroMedico.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class FotoRegistro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
